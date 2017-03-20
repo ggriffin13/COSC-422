7464b83bc2041a924186cc6e8112018a942dd473
Griffin Obeid <griffobeid@gmail.com>
2017-03-20 15:36:23 -0400

    Changed up the Infrared flasher program. Now the program runs on a subroutine that activates with a pull up resistor

--- Assignment4-FlashIR.asm
+++ Assignment4-FlashIR.asm
@@ -1,22 +1,20 @@
+;Very simple program to flash infrared 38,000 times
+;Pull up resistor on RA1
+;Infrared LED on any pin on PORTB
+
 	LIST	p=16F628		;tell assembler what chip we are using
 	include "P16F628.inc"		;include the defaults for the chip
 	__config 0x3D18			;sets the configuration settings (oscillator type etc.)
 
 	cblock 	0x20 			;start of general purpose registers
 	endc
-	; Very short stuff here
-;     code for generating timed delays!!
-IR_PORT Equ PORTB
-IR_OUT	Equ 0x01
-	
+
 	org	0x0000			;org sets the origin, 0x0000 for the 16F628,
-					;this is where the program starts running	
+					;this is where the program starts running
 	movlw	0x07
 	movwf	CMCON			;turn comparators off (make it like a 16F84)
-	
-; wanna generate some delays ??
-	
-	bsf	STATUS,RP0		
+
+	bsf	STATUS,RP0
 	movlw	0x00
 	movwf	TRISB		; Port B is output
 	movlw	0xff
@@ -24,6 +22,11 @@ IR_OUT	Equ 0x01
 	bcf	STATUS,RP0	; back to bank 0
 
 top
+	btfss	PORTA, 1
+	call	flashIR
+	goto	top
+
+flashIR
 	movlw 0x00
 	movwf PORTB
 	nop
@@ -47,6 +50,8 @@ top
 	nop
 	nop
 	nop
-	nop
-	goto	top
+	btfss	PORTA, 1
+	goto 	flashIR
+	return
+
 	end
