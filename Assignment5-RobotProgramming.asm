4dbee3ae11d840d4491237d2dfe8398e2ce2e7cf
Griffin Obeid <griffobeid@gmail.com>
2017-03-22 13:52:38 -0400

    Finished Assignment5. The robot now moves forward. After hitting something with the antennas it travels backwards, and then turns. After turning it proceeds forward.

--- Assignment5-RobotProgramming.asm
+++ Assignment5-RobotProgramming.asm
@@ -1,265 +1,160 @@
 ;Griffin Obeid
-;Make the robot move forward using TMR0 and TMR2
-;Some code borrowed from the second group of grad Students to present.
-;
-;In this program TMR0 deals with one wheel
-;While TMR2 is on the other wheel.
-;The main program body (loop) will deal with the sensors
+;3/22/2017
+;===============================================================================
+;This program moves the boe-bot forward until the antenna sensors hit something 
+;then it moves backwards shortly, turns to the right ~ 90 degrees, and then
+;continues moving forward.
+;===============================================================================
+; uncomment following two lines if using 16f627 or 16f628.
+    LIST    p=16F628	;tell assembler what chip we are using
+    include "P16F628.inc"	;include the defaults for the chip
+    __config 0x3D18		;sets the configuration settings
 
-	LIST	 p=16F628	;tell assembler what chip we are using
-	include  "P16F628.inc"	;include the defaults for the chip
-	__config 0x3D18		;sets the configuration settings
-
-cblock 	0x20 			;start of general purpose registers
-
-	controlR		;counter to toggle between 20ms and 1ms
-	controlL		;counter to toggle between 20ms and 2ms
-	CounterA
-	CounterB
-	CounterC
-
+    cblock 	0x20 		;start of general purpose registers
+	counterA		;counterA is used in delay routines
+	counterB		;counterB is used in delay routines
+	reverseTime	
+	numTurns	
 	endc
 
+;same setup for every program using interrupts
 	org	0x00
 	goto	main
-	org	0x04		;as usual, an interrupt sets the PC to 0x04
+	org	0x04
 	goto	isr
 
 main
-	;turn comparators off (make it like a 16F84)
-	movlw	0x07
-	movwf	CMCON
+;turn comparators off (make it like a 16F84)
+    movlw   0x07
+    movwf   CMCON			
+		
+;turn B0 interrupts on (peripheral)
+    bsf	    INTCON, GIE		;enable interrupts
+    bsf	    INTCON, INTE	;B0 is the interrupt line
+	
+;set up the I/O
+    bsf	    STATUS,RP0
+    bcf	    0x81, INTEDG	;falling edge interrupts
+    movlw   b'00000001'
+    movwf   TRISB		;PORTB7:1 = output, PORTB0 = input
+    movlw   b'11111000'
+    movwf   TRISA		;PORTA7:3 = input, PORTA2:0 = output
+    bcf	    STATUS,RP0		;return to bank 0
 
-;in OPTION_REG:
-;bit 5 - enable TMR0
-;bit 3 - assign prescaler to TMR0
-;bits 2:0 - set prescaler to 1:128
-	banksel	OPTION_REG
-	movlw	b'10000110'
-	movwf	OPTION_REG
+;Program Body:
+			
+driveLoop	
+    call    goForward
+    goto    driveLoop		;Lets  go forward until we hit something
 
-;in T2CON:
-;bits 6:3 - set postscaler to 1:8
-;bit 2 - turn TMR2 on
-;bits 1:0 - set prescaler to 1:16
-	banksel	T2CON
-	movlw	b'01110111'
-	movwf	T2CON
+;Subroutine: goForward
+;Robot will move forwards
+;Pulse right wheel for 1ms and left wheel for 2ms
+;Left wheel = RA2 & Right wheel = RA1
+goForward	
+    bsf	    PORTB, 4
+    movlw   b'00000110'
+    movwf   PORTA		;runs both servos as high
+    call    wait1ms		;for 1ms
+			
+    bcf	    PORTA, 1		;turns off right servo
+    call    wait1ms		;for 1ms
 
-;enable TMR2 interrupt
-	banksel	PIE1
-	bsf	PIE1,TMR2IE
+    bcf	    PORTA, 2		;turns off the left servo
 
-;bit 7 - enable global interrupt (GIE)
-;bit 6 - enable peripheral interrupt
-;bit 5 - enable TMR0 interrupt
-	movlw	b'11100000'
-	movwf	INTCON
+    call    waiter		;waits for the rest of the required time
+    return
 
-	bsf	STATUS,RP0		;select bank 1
-	movlw	b'11110000'		
-	movwf	TRISB			;portb 7:4 input, 3:0 output
-	movlw	b'11111000'
-	movwf	TRISA			;Porta 7:3 input, 2:0 output
-	bcf	STATUS,RP0		;return to bank 0
+;Subroutine: goBackward
+;Robot will move backwards
+;Pulse right wheel for 2ms and left wheel for 1ms
+;Left wheel = RA2 & Right wheel = RA1
+goBackward
+    movlw   0x50
+    movwf   reverseTime
 
-;PROGRAM BODY
-;Movement of the wheels is controlled entirely through
-;interrupts, leaving our program body free to deal with anything else really
-;RA1 has right wheel | RA2 has left wheel
-top
-	call 	init_Servos
-loop
-	btfss	PORTB, 0		;RB0 has antenna sensors
-	call	Reverse_and_turn
-	goto	loop
+backward	
+    movlw   b'00000110'	
+    movwf   PORTA		;runs both servos as high
+    call    wait1ms		;for 1ms
+			
+    bcf	    PORTA, 2		;turns off left servo
+    call    wait1ms		;for 1ms
 
-;isr
-;SUBROUTINE
-;Checks INTCON,2 (TMR0 flag) and PIR1,1 (TMR2 flag)
-;to determine which threw the interrupt and calls
-;appropriate subroutine.
+    bcf	    PORTA, 1		;turns off the right servo
+
+    call    waiter		;waits for the rest of the required time
+    decfsz  reverseTime, f
+    goto    backward
+			
+    return
+
+;Subroutine: turn
+;Turns the robot approximately 90 degrees to the right
+turn	
+    movlw   0x7			;decrementer for timing the angle of the turn
+    movwf   numTurns
+rightStart	
+    movlw   b'00000110'
+    movwf   PORTA
+    call    wait1ms
+    call    wait1ms		;waits for 2ms (forward full speed for both)
+
+    movlw   0x00
+    movwf   PORTA
+    
+    call    waiter
+    decfsz  numTurns, f
+    goto    rightStart
+			
+    return
+
+;Subroutine: waiter
+;Waits for about 20ms for inbetween pulses on the servos motors
+waiter 		
+    movlw   0x3A		;20ms 
+    movwf   counterA
+    movlw   0x10
+    movwf   counterB
+waiter_in	
+    decfsz  counterA, f
+    goto    $+2
+    decfsz  counterB, f
+    goto    waiter_in
+			
+    goto    $+1			;3 cycles
+    nop
+			
+    return			;4 cycles
+			
+;Subroutine: wait1ms
+;Wait for 1 ms so that we can pulse our servos motors high for the correct
+;amount of time.	
+wait1ms		
+    movlw   0xC6		;1 ms
+    movwf   counterA
+    movlw   0x01
+    movwf   counterB
+wait1ms_in	
+    decfsz  counterA, f
+    goto    $+2
+    decfsz  counterB, f
+    goto    wait1ms_in
+						
+    goto    $+1			;3 cycles
+    nop
+			
+    return			;4 cycles
+			
+;Interrupt Subroutine
 isr
-	btfsc	INTCON,2		;check TMR0 interrupt flag
-	call 	Right_wheel
-	btfsc	PIR1,1			;check TMR2 interrupt flag
-	call 	Left_wheel
+    call    goBackward		;lets go back
+    
+    call    turn		;now lets turn		
+    goto    endIsr		;back to going forward
 
-	retfie
-	
-;isr2
-;SUBROUTINE
-;Same as isr but go in reverse instead
-isr2
-	btfsc	INTCON,2		;check TMR0 interrupt flag
-	call 	Right_wheel_reverse
-	btfsc	PIR1,1			;check TMR2 interrupt flag
-	call 	Left_wheel_reverse
-
-	retfie
-	
-;isr3
-;SUBROUTINE
-;Same as isr but turns the dude instead
-isr3
-	btfsc	INTCON,2		;check TMR0 interrupt flag
-	call 	Right_wheel_reverse
-	btfsc	PIR1,1			;check TMR2 interrupt flag
-	call 	Left_wheel
-
-	retfie
-
-;Right_wheel
-;SUBROUTINE
-;Toggles RA1 between low for 20ms and high for 1ms.
-;Bit 0 of controlR is used as a flag.
-Right_wheel
-	bcf	INTCON,2
-
-	btfss	controlR,0
-	bsf	PORTA,1
-	btfsc	controlR,0
-	bcf	PORTA,1
-
-	btfss	controlR,0		;modify value in TMR0
-	movlw	d'248'			;1 ms
-	btfsc	controlR,0
-	movlw	d'100'			;20 ms
-	movwf	TMR0
-	incf	controlR		;increment controlR (toggle "flag")
-
-	retfie
-
-;Right_wheel_reverse
-;SUBROUTINE
-;Toggles RA1 between low for 20ms and high for 2ms.
-;Bit 0 of controlR is used as a flag.
-Right_wheel_reverse
-	bcf	INTCON,2
-
-	btfss	controlR,0
-	bsf	PORTA,1
-	btfsc	controlR,0
-	bcf	PORTA,1
-
-	btfss	controlR,0		;modify value in TMR0
-	movlw	d'240'			;2 ms
-	btfsc	controlR,0
-	movlw	d'100'			;20 ms
-	movwf	TMR0
-	incf	controlR		;increment controlR (toggle "flag")
-
-	retfie
-
-;Left_wheel
-;SUBROUTINE
-;Toggles RA2 between low for 20ms and high for 2ms.
-;Bit 0 of controlL is used as a flag.
-Left_wheel
-	bcf	PIR1,1
-
-	btfss	controlL,0
-	bsf	PORTA,2
-	btfsc	controlL,0
-	bcf	PORTA,2
-
-	btfss	controlL,0		;modify value in PR2
-	movlw	d'16'			;2 ms
-	btfsc	controlL,0
-	movlw	d'156'			;20 ms
-
-	banksel	PR2
-	movwf	PR2
-	bcf	STATUS,RP0
-	incf	controlL		;increment controlL (toggle "flag")
-
-	retfie
-
-;Left_wheel_reverse
-;SUBROUTINE
-;Toggles RA2 between low for 20ms and high for 1ms.
-;Bit 0 of controlL is used as a flag.
-Left_wheel_reverse
-	bcf	PIR1,1
-
-	btfss	controlL,0
-	bsf	PORTA,2
-	btfsc	controlL,0
-	bcf	PORTA,2
-
-	btfss	controlL,0		;modify value in PR2
-	movlw	d'8'			;1 ms
-	btfsc	controlL,0
-	movlw	d'156'			;20 ms
-
-	banksel	PR2
-	movwf	PR2
-	bcf	STATUS,RP0
-	incf	controlL		;increment controlL (toggle "flag")
-
-	retfie
-
-;Reverse_and_turn
-;SUBROUTINE
-;This subroutine gets called when the antenna sensor rams into the wall
-;Reverses the dude, turns the dude, then dude starts moving forward again
-Reverse_and_turn
-	goto	init_Servos
-	goto	isr2
-	
-	call	DelayOneSecond
-	call	DelayOneSecond
-	
-	goto	init_Servos
-	goto	isr3
-	
-	call	DelayOneSecond
-	
-	goto	init_Servos
-	goto	isr
-	return
-
-;init_Servos
-;SUBROUTINE
-;Must Be called before the motor starts back up
-init_Servos
-	clrf	controlR
-	clrf	controlL
-
-	;set RA1 low and start TMR0 at 100
-	;(256 - 100) * 128 ~ 20,000?, or 20ms
-	bcf	PORTA,1
-	movlw	d'100'
-	movwf	TMR0
-
-	;TMR2 resets when it matches value in PR2
-	;set PR2 to 156
-	;156 * 128 ~ 20,000?, or 20ms
-	banksel	PR2
-	movlw	d'156'
-	movwf	PR2
-	bcf	STATUS,RP0		;return to bank 0
-
-	bcf	PORTA,2			;set RA2 low
-	return
-
-;DelayOneSecond
-;SUBROUTINE DELAY
-;Timed delay for 1 second
-DelayOneSecond
-	movlw	d'6'
-	movwf	CounterC
-	movlw	d'24'
-	movwf	CounterB
-	movlw	d'168'
-	movwf	CounterA
-keepGoing
-	decfsz	CounterA, 1
-	goto	keepGoing
-	decfsz	CounterB, 1
-	goto	keepGoing
-	decfsz	CounterC, 1
-	goto	keepGoing
-	return
+endIsr		
+    bcf	    INTCON,INTF		;reset the interrupt	
+    retfie
 
 	end
